0868c81a4fbf6222ebca0c0cc92fe017f1a31cb4
reckless007 <shekhar.divyans@gmail.com>
2020-10-09 10:04:24 +0530

    first commit

---
+++ Login.js
@@ -0,0 +1,40 @@
+function validation() {
+    let name = document.logins.uname.value;
+    let password = document.logins.pwd.value;
+
+    if (password.length < 6) {
+        alert("Password is too short");
+        return;
+    }
+
+    if (name.length < 6) {
+        alert("Username Should not be less than 5 Character");
+        return;
+    }
+}
+
+
+function vali() {
+    let name = document.registers.uname1.value;
+    let password = document.registers.pwd1.value;
+    let mail = document.registers.mail.value;
+
+
+    if (password.length < 6) {
+        alert("Password is too short");
+        return;
+    }
+
+    if (name.length < 6) {
+        alert("Username Should not be less than 5 Character");
+        return;
+    }
+    var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+
+    if (!mail.match(reEmail)) {
+        alert("Invalid email address");
+        return;
+    }
+
+
+}
